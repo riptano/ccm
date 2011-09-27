47dd4cf2800912d566830edbe9d2c37cfc188fd2
Sylvain Lebresne <sylvain@datastax.com>
2011-09-27 12:21:00 +0200

    Improve cli handling

--- ccm_lib/cli_session.py
+++ ccm_lib/cli_session.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 from threading  import Thread
 try:
     from Queue import Queue, Empty
@@ -17,26 +17,34 @@ class CliSession():
         for t in [ self.thread_out, self.thread_err ]:
             t.daemon = True
             t.start()
-        self.outputs = []
-        self.errors = []
+        self.__outputs = []
+        self.__errors = []
 
     def do(self, query):
         # Reads whatever remains in stdout/stderr
-        self.__read(self.stdout)
-        self.__read(self.stderr)
+        self.__read_all()
         self.process.stdin.write(query + ';\n')
-        self.outputs.append(self.__read(self.stdout))
-        self.errors.append(self.__read(self.stderr))
         return self
 
     def last_output(self):
-        return self.outputs[-1]
+        self.__read_output()
+        return self.__outputs[-1]
 
     def last_error(self):
-        return self.errors[-1]
+        self.__read_errors()
+        return self.__errors[-1]
+
+    def outputs(self):
+        self.__read_output()
+        return self.__outputs
+
+    def errors(self):
+        self.__read_errors()
+        return self.__errors
 
     def has_errors(self):
-        for err in self.errors:
+        self.__read_errors()
+        for err in self.__errors:
             if err != '':
                 return True
         return False
@@ -45,15 +53,29 @@ class CliSession():
         self.process.stdin.write('quit;\n')
         self.process.wait()
 
+    def __read_all(self):
+        self.__read_output()
+        self.__read_errors()
+
+    def __read_output(self):
+        r = self.__read(self.stdout)
+        if r:
+            self.__outputs.append(r)
+
+    def __read_errors(self):
+        r = self.__read(self.stderr)
+        if r:
+            self.__errors.append(r)
+
     def __read(self, queue):
-        output = ''
+        output = None
         while True:
             try:
-                line = queue.get(timeout=.1)
+                line = queue.get(timeout=.2)
             except Empty:
                 return output
             else:
-                output = output + line
+                output = line if output is None else output + line
 
     def __enqueue_output(self, out, queue):
         for line in iter(out.readline, ''):
