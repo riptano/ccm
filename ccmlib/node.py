70040260335bec2048d7034c88b8f9d99cb32d0b
Philip Thompson <ptnapoleon@gmail.com>
2014-12-12 11:16:45 -0500

    Forward any existing JVM_EXTRA_OPTS

--- ccmlib/node.py
+++ ccmlib/node.py
@@ -474,7 +474,7 @@ class Node(object):
             args.append('-Dcassandra.replace_address=%s' % str(replace_address))
         if use_jna is False:
             args.append('-Dcassandra.boot_without_jna=true')
-        env['JVM_EXTRA_OPTS'] = " ".join(jvm_args)
+        env['JVM_EXTRA_OPTS'] = env.get('JVM_EXTRA_OPTS', "") + " ".join(jvm_args)
 
         process = None
         if common.is_win():
