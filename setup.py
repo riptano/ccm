8765abca892c7a85e1d7e135c05555aa74ca3458
Philip Thompson <ptnapoleon@gmail.com>
2015-02-17 21:33:35 -0500

    Revert "Rename ccm to ccm.py"
    This reverts commit 7db193df114d705478ed958e3b5a345291e10f31.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author_email='sylvain@datastax.com',
     url='https://github.com/pcmanus/ccm',
     packages=['ccmlib', 'ccmlib.cmds'],
-    scripts=['ccm.py'],
+    scripts=['ccm'],
     install_requires=['pyYaml', 'six >=1.4.1'],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
