b555ab4ef87e5af28e5558fe1e72794ddc68d228
Philip Thompson <ptnapoleon@gmail.com>
2015-10-01 15:35:27 -0400

    Updated version to 2.0.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if system() == "Windows":
 
 setup(
     name='ccm',
-    version='2.0.4.1',
+    version='2.0.5',
     description='Cassandra Cluster Manager',
     long_description=open(abspath(join(dirname(__file__), 'README.md'))).read(),
     author='Sylvain Lebresne',
