737d749cf8d3ceac2cf198c3b490636e60e22695
Philip Thompson <ptnapoleon@gmail.com>
2015-12-02 16:31:25 -0500

    Cut a 2.0.6 release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if system() == "Windows":
 
 setup(
     name='ccm',
-    version='2.0.5',
+    version='2.0.6',
     description='Cassandra Cluster Manager',
     long_description=open(abspath(join(dirname(__file__), 'README.md'))).read(),
     author='Sylvain Lebresne',
