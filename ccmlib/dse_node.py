riptano/ccm

commit f9c70c078f76441cd8cae4c1576a658ca07cbb47

Allow 3rd party extensions to be installed

ccmlib/dse_node.py
@@ -12,7 +12,7 @@ import time
 import yaml
 from six import print_,iteritems
 
-from ccmlib import common
+from ccmlib import common, extension
 from ccmlib.node import Node, NodeError, NodetoolError
 
 
@@ -91,77 +91,15 @@ class DseNode(Node):
         """
         super(DseNode, self).watch_log_for_alive(nodes, from_mark=from_mark, timeout=timeout, filename=filename)
 
-    def start(self,
-              join_ring=True,
-              no_wait=False,
-              verbose=False,
-              update_pid=True,
-              wait_other_notice=False,
-              replace_token=None,
-              replace_address=None,
-              jvm_args=None,
-              wait_for_binary_proto=False,
-              profile_options=None,
-              use_jna=False,
-              quiet_start=False,
-              allow_root=False):
-        """
-        Start the node. Options includes:
-          - join_ring: if false, start the node with -Dcassandra.join_ring=False
-          - no_wait: by default, this method returns when the node is started and listening to clients.
-            If no_wait=True, the method returns sooner.
-          - wait_other_notice: if True, this method returns only when all other live node of the cluster
-            have marked this node UP.
-          - replace_token: start the node with the -Dcassandra.replace_token option.
-          - replace_address: start the node with the -Dcassandra.replace_address option.
-        """
-        if jvm_args is None:
-            jvm_args = []
-
-        if self.is_running():
-            raise NodeError("%s is already running" % self.name)
-
-        for itf in list(self.network_interfaces.values()):
-            if itf is not None and replace_address is None:
-                common.check_socket_available(itf)
-
-        if wait_other_notice:
-            marks = [(node, node.mark_log()) for node in list(self.cluster.nodes.values()) if node.is_running()]
-
-        self.mark = self.mark_log()
-
+    def get_launch_bin(self):
         cdir = self.get_install_dir()
         launch_bin = common.join_bin(cdir, 'bin', 'dse')
         # Copy back the dse scripts since profiling may have modified it the previous time
         shutil.copy(launch_bin, self.get_bin_dir())
-        launch_bin = common.join_bin(self.get_path(), 'bin', 'dse')
+        return common.join_bin(self.get_path(), 'bin', 'dse')
 
-        # If Windows, change entries in .bat file to split conf from binaries
-        if common.is_win():
-            self.__clean_bat()
-
-        if profile_options is not None:
-            config = common.get_config()
-            if 'yourkit_agent' not in config:
-                raise NodeError("Cannot enable profile. You need to set 'yourkit_agent' to the path of your agent in a {0}/config".format(common.get_default_path_display_name()))
-            cmd = '-agentpath:%s' % config['yourkit_agent']
-            if 'options' in profile_options:
-                cmd = cmd + '=' + profile_options['options']
-            print_(cmd)
-            # Yes, it's fragile as shit
-            pattern = r'cassandra_parms="-Dlog4j.configuration=log4j-server.properties -Dlog4j.defaultInitOverride=true'
-            common.replace_in_file(launch_bin, pattern, '    ' + pattern + ' ' + cmd + '"')
-
-        os.chmod(launch_bin, os.stat(launch_bin).st_mode | stat.S_IEXEC)
-
-        env = self.get_env()
-
-        if common.is_win():
-            self._clean_win_jmx()
-
-        pidfile = os.path.join(self.get_path(), 'cassandra.pid')
-        args = [launch_bin, 'cassandra']
-
+    def add_custom_launch_arguments(self, args):
+        args.append('cassandra')
         for workload in self.workloads:
             if 'hadoop' in workload:
                 args.append('-t')
@@ -174,66 +112,58 @@ class DseNode(Node):
             if 'graph' in workload:
                 args.append('-g')
 
-        args += ['-p', pidfile, '-Dcassandra.join_ring=%s' % str(join_ring)]
-        args += ['-Dcassandra.logdir=%s' % os.path.join(self.get_path(), 'logs')]
-        if replace_token is not None:
-            args.append('-Dcassandra.replace_token=%s' % str(replace_token))
-        if replace_address is not None:
-            args.append('-Dcassandra.replace_address=%s' % str(replace_address))
-        if use_jna is False:
-            args.append('-Dcassandra.boot_without_jna=true')
-        if allow_root:
-            args.append('-R')
-        args = args + jvm_args
-
-        self._delete_old_pid()
-
-        process = None
-
-        FNULL = open(os.devnull, 'w')
-        stdout_sink = subprocess.PIPE if verbose else FNULL
-
-        if common.is_win():
-            # clean up any old dirty_pid files from prior runs
-            if (os.path.isfile(self.get_path() + "/dirty_pid.tmp")):
-                os.remove(self.get_path() + "/dirty_pid.tmp")
-            process = subprocess.Popen(args, cwd=self.get_bin_dir(), env=env, stdout=FNULL, stderr=subprocess.PIPE)
-        else:
-            process = subprocess.Popen(args, env=env, stdout=stdout_sink, stderr=subprocess.PIPE)
-
-        if verbose:
-            stdout, stderr = process.communicate()
-            print_(stdout)
-            print_(stderr)
-
-        # Our modified batch file writes a dirty output with more than just the pid - clean it to get in parity
-        # with *nix operation here.
-        if common.is_win():
-            self.__clean_win_pid()
-            self._update_pid(process)
-        elif update_pid:
-            self._update_pid(process)
-
-            if not self.is_running():
-                raise NodeError("Error starting node %s" % self.name, process)
-
-        if wait_other_notice:
-            for node, mark in marks:
-                node.watch_log_for_alive(self, from_mark=mark)
-
-        if wait_for_binary_proto:
-            self.wait_for_binary_interface(from_mark=self.mark)
-
+    def start(self,
+              join_ring=True,
+              no_wait=False,
+              verbose=False,
+              update_pid=True,
+              wait_other_notice=True,
+              replace_token=None,
+              replace_address=None,
+              jvm_args=None,
+              wait_for_binary_proto=False,
+              profile_options=None,
+              use_jna=False,
+              quiet_start=False,
+              allow_root=False,
+              set_migration_task=True):
+        process = super(DseNode, self).start(join_ring, no_wait, verbose, update_pid, wait_other_notice, replace_token,
+                                             replace_address, jvm_args, wait_for_binary_proto, profile_options, use_jna,
+                                             quiet_start, allow_root, set_migration_task)
         if self.cluster.hasOpscenter():
             self._start_agent()
 
-        return process
+
+    def _start_agent(self):
+        agent_dir = os.path.join(self.get_path(), 'datastax-agent')
+        if os.path.exists(agent_dir):
+            self._write_agent_address_yaml(agent_dir)
+            self._write_agent_log4j_properties(agent_dir)
+            args = [os.path.join(agent_dir, 'bin', common.platform_binary('datastax-agent'))]
+            subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
     def stop(self, wait=True, wait_other_notice=False, gently=True):
-        stopped = super(DseNode, self).stop(wait, wait_other_notice, gently)
         if self.cluster.hasOpscenter():
             self._stop_agent()
-        return stopped
+        return super(DseNode, self).stop(wait, wait_other_notice, gently)
+
+
+    def _stop_agent(self):
+        agent_dir = os.path.join(self.get_path(), 'datastax-agent')
+        if os.path.exists(agent_dir):
+            pidfile = os.path.join(agent_dir, 'datastax-agent.pid')
+        if os.path.exists(pidfile):
+            with open(pidfile, 'r') as f:
+                pid = int(f.readline().strip())
+                f.close()
+            if pid is not None:
+                try:
+                    os.kill(pid, signal.SIGKILL)
+                except OSError:
+                    pass
+            os.remove(pidfile)
+
 
     def nodetool(self, cmd, username=None, password=None, capture_output=True, wait=True):
         """
@@ -267,6 +197,7 @@ class DseNode(Node):
 
     def dsetool(self, cmd):
         env = self.get_env()
+        extension.append_to_client_env(self, env)
         dsetool = common.join_bin(self.get_install_dir(), 'bin', 'dsetool')
         args = [dsetool, '-h', 'localhost', '-j', str(self.jmx_port)]
         args += cmd.split()
@@ -277,6 +208,7 @@ class DseNode(Node):
         if dse_options is None:
             dse_options = []
         env = self.get_env()
+        extension.append_to_client_env(self, env)
         env['JMX_PORT'] = self.jmx_port
         dse = common.join_bin(self.get_install_dir(), 'bin', 'dse')
         args = [dse]
@@ -488,29 +420,6 @@ class DseNode(Node):
         agent_target = os.path.join(self.get_path(), 'datastax-agent')
         if os.path.exists(agent_source) and not os.path.exists(agent_target):
             shutil.copytree(agent_source, agent_target)
-
-    def _start_agent(self):
-        agent_dir = os.path.join(self.get_path(), 'datastax-agent')
-        if os.path.exists(agent_dir):
-            self._write_agent_address_yaml(agent_dir)
-            self._write_agent_log4j_properties(agent_dir)
-            args = [os.path.join(agent_dir, 'bin', common.platform_binary('datastax-agent'))]
-            subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    def _stop_agent(self):
-        agent_dir = os.path.join(self.get_path(), 'datastax-agent')
-        if os.path.exists(agent_dir):
-            pidfile = os.path.join(agent_dir, 'datastax-agent.pid')
-            if os.path.exists(pidfile):
-                with open(pidfile, 'r') as f:
-                    pid = int(f.readline().strip())
-                    f.close()
-                if pid is not None:
-                    try:
-                        os.kill(pid, signal.SIGKILL)
-                    except OSError:
-                        pass
-                os.remove(pidfile)
 
     def _write_agent_address_yaml(self, agent_dir):
         address_yaml = os.path.join(agent_dir, 'conf', 'address.yaml')

ccm
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-
+import pkg_resources
 from six import print_
 
 from ccmlib import common
@@ -31,6 +31,7 @@ def print_global_usage():
             print_("Internal error, unknown command {0}".format(cmd_name))
             exit(1)
         print_("  {0:14} {1}".format(cmd_name, cmd.description()))
+    print_("")
     print_("or <node_name> is the name of a node of the current cluster and <node_cmd> is one of")
     for cmd_name in node_cmds.node_cmds():
         cmd = get_command("node", cmd_name)
@@ -39,6 +40,9 @@ def print_global_usage():
             exit(1)
         print_("  {0:14} {1}".format(cmd_name, cmd.description()))
     exit(1)
+
+for entry_point in pkg_resources.iter_entry_points(group='ccm_extension'):
+    entry_point.load()()
 
 common.check_win_requirements()
 

ccmlib/cluster.py
@@ -13,7 +13,7 @@ from collections import OrderedDict, defaultdict
 import yaml
 from six import iteritems, print_
 
-from ccmlib import common, repository
+from ccmlib import common, repository, extension
 from ccmlib.node import Node, NodeError
 from six.moves import xrange
 
@@ -365,6 +365,8 @@ class Cluster(object):
         if jvm_args is None:
             jvm_args = []
 
+        extension.pre_cluster_start(self)
+
         common.assert_jdk_valid_for_cassandra_version(self.cassandra_version())
 
         # check whether all loopback aliases are available before starting any nodes
@@ -420,13 +422,17 @@ class Cluster(object):
                 for node, p, mark in started:
                     node.wait_for_binary_interface(process=p, verbose=verbose, from_mark=mark)
 
+        extension.post_cluster_start(self)
+
         return started
 
     def stop(self, wait=True, gently=True):
         not_running = []
+        extension.pre_cluster_stop(self);
         for node in list(self.nodes.values()):
             if not node.stop(wait, gently=gently):
                 not_running.append(node)
+        extension.post_cluster_stop(self)
         return not_running
 
     def set_log_level(self, new_level, class_names=None):
@@ -577,20 +583,22 @@ class Cluster(object):
         node_list = [node.name for node in list(self.nodes.values())]
         seed_list = self.get_seeds()
         filename = os.path.join(self.__path, self.name, 'cluster.conf')
+        config_map = {
+            'name': self.name,
+            'nodes': node_list,
+            'seeds': seed_list,
+            'partitioner': self.partitioner,
+            'install_dir': self.__install_dir,
+            'config_options': self._config_options,
+            'dse_config_options': self._dse_config_options,
+            'log_level': self.__log_level,
+            'use_vnodes': self.use_vnodes,
+            'datadirs': self.data_dir_count,
+            'environment_variables': self._environment_variables
+        }
+        extension.append_to_cluster_config(self, config_map)
         with open(filename, 'w') as f:
-            yaml.safe_dump({
-                'name': self.name,
-                'nodes': node_list,
-                'seeds': seed_list,
-                'partitioner': self.partitioner,
-                'install_dir': self.__install_dir,
-                'config_options': self._config_options,
-                'dse_config_options': self._dse_config_options,
-                'log_level': self.__log_level,
-                'use_vnodes': self.use_vnodes,
-                'datadirs': self.data_dir_count,
-                'environment_variables': self._environment_variables
-            }, f)
+            yaml.safe_dump(config_map, f)
 
     def __update_pids(self, started):
         for node, p, _ in started:

ccmlib/cluster_factory.py
@@ -2,7 +2,7 @@ import os
 
 import yaml
 
-from ccmlib import common, repository
+from ccmlib import common, repository, extension
 from ccmlib.cluster import Cluster
 from ccmlib.dse_cluster import DseCluster
 from ccmlib.node import Node
@@ -43,6 +43,7 @@ class ClusterFactory():
                 cluster.use_vnodes = data['use_vnodes']
             if 'datadirs' in data:
                 cluster.data_dir_count = int(data['datadirs'])
+            extension.load_from_cluster_config(cluster, data)
         except KeyError as k:
             raise common.LoadError("Error Loading " + filename + ", missing property:" + k)
 

ccmlib/cmds/cluster_cmds.py
@@ -14,9 +14,7 @@ from ccmlib.dse_cluster import DseCluster
 from ccmlib.dse_node import DseNode
 from ccmlib.node import Node, NodeError
 
-
-def cluster_cmds():
-    return [
+CLUSTER_CMDS = [
         "create",
         "add",
         "populate",
@@ -47,6 +45,8 @@ def cluster_cmds():
         "setworkload"
     ]
 
+def cluster_cmds():
+    return CLUSTER_CMDS
 
 def parse_populate_count(v):
     if v is None:

ccmlib/cmds/node_cmds.py
@@ -8,54 +8,55 @@ from ccmlib import common
 from ccmlib.cmds.command import Cmd
 from ccmlib.node import NodeError
 
+NODE_CMDS = [
+    "show",
+    "remove",
+    "showlog",
+    "setlog",
+    "start",
+    "stop",
+    "ring",
+    "flush",
+    "compact",
+    "drain",
+    "cleanup",
+    "repair",
+    "scrub",
+    "verify",
+    "shuffle",
+    "sstablesplit",
+    "getsstables",
+    "decommission",
+    "json",
+    "updateconf",
+    "updatelog4j",
+    "stress",
+    "cli",
+    "cqlsh",
+    "scrub",
+    "verify",
+    "status",
+    "setdir",
+    "bulkload",
+    "version",
+    "nodetool",
+    "dsetool",
+    "setworkload",
+    "dse",
+    "hadoop",
+    "hive",
+    "pig",
+    "sqoop",
+    "spark",
+    "pause",
+    "resume",
+    "jconsole",
+    "versionfrombuild",
+    "byteman"
+]
 
 def node_cmds():
-    return [
-        "show",
-        "remove",
-        "showlog",
-        "setlog",
-        "start",
-        "stop",
-        "ring",
-        "flush",
-        "compact",
-        "drain",
-        "cleanup",
-        "repair",
-        "scrub",
-        "verify",
-        "shuffle",
-        "sstablesplit",
-        "getsstables",
-        "decommission",
-        "json",
-        "updateconf",
-        "updatelog4j",
-        "stress",
-        "cli",
-        "cqlsh",
-        "scrub",
-        "verify",
-        "status",
-        "setdir",
-        "bulkload",
-        "version",
-        "nodetool",
-        "dsetool",
-        "setworkload",
-        "dse",
-        "hadoop",
-        "hive",
-        "pig",
-        "sqoop",
-        "spark",
-        "pause",
-        "resume",
-        "jconsole",
-        "versionfrombuild",
-        "byteman"
-    ]
+    return NODE_CMDS
 
 
 class NodeShowCmd(Cmd):

ccmlib/extension.py
@@ -1,0 +1,45 @@
+PRE_CLUSTER_START_HOOKS = []
+POST_CLUSTER_START_HOOKS = []
+PRE_CLUSTER_STOP_HOOKS = []
+POST_CLUSTER_STOP_HOOKS = []
+APPEND_TO_CLUSTER_CONFIG_HOOKS = []
+LOAD_FROM_CLUSTER_CONFIG_HOOKS = []
+APPEND_TO_SERVER_ENV_HOOKS = []
+APPEND_TO_CLIENT_ENV_HOOKS = []
+APPEND_TO_CQLSH_ARGS_HOOKS = []
+
+def pre_cluster_start(cluster):
+    for hook in PRE_CLUSTER_START_HOOKS:
+        hook(cluster)
+
+def post_cluster_start(cluster):
+    for hook in POST_CLUSTER_START_HOOKS:
+        hook(cluster)
+
+def pre_cluster_stop(cluster):
+    for hook in PRE_CLUSTER_STOP_HOOKS:
+        hook(cluster)
+
+def post_cluster_stop(cluster):
+    for hook in POST_CLUSTER_STOP_HOOKS:
+        hook(cluster)
+
+def append_to_cluster_config(cluster, config_map):
+    for hook in APPEND_TO_CLUSTER_CONFIG_HOOKS:
+        hook(cluster, config_map)
+
+def load_from_cluster_config(cluster, config_map):
+    for hook in LOAD_FROM_CLUSTER_CONFIG_HOOKS:
+        hook(cluster, config_map)
+
+def append_to_server_env(node, env):
+    for hook in APPEND_TO_SERVER_ENV_HOOKS:
+        hook(node, env)
+
+def append_to_client_env(node, env):
+    for hook in APPEND_TO_CLIENT_ENV_HOOKS:
+        hook(node, env)
+
+def append_to_cqlsh_args(node, env, args):
+    for hook in APPEND_TO_CQLSH_ARGS_HOOKS:
+        hook(node, env, args)

ccmlib/node.py
@@ -18,7 +18,7 @@ from datetime import datetime
 import yaml
 from six import iteritems, print_, string_types
 
-from ccmlib import common
+from ccmlib import common, extension
 from ccmlib.cli_session import CliSession
 from ccmlib.repository import setup
 from six.moves import xrange
@@ -496,6 +496,16 @@ class Node(object):
             warnings.warn("Binary interface %s:%s is not listening after 10 seconds, node may have failed to start."
                           % (binary_itf[0], binary_itf[1]))
 
+    def get_launch_bin(self):
+        cdir = self.get_install_dir()
+        launch_bin = common.join_bin(cdir, 'bin', 'cassandra')
+        # Copy back the cassandra scripts since profiling may have modified it the previous time
+        shutil.copy(launch_bin, self.get_bin_dir())
+        return common.join_bin(self.get_path(), 'bin', 'cassandra')
+
+    def add_custom_launch_arguments(self, args):
+        pass
+
     def start(self,
               join_ring=True,
               no_wait=False,
@@ -546,11 +556,7 @@ class Node(object):
 
         self.mark = self.mark_log()
 
-        cdir = self.get_install_dir()
-        launch_bin = common.join_bin(cdir, 'bin', 'cassandra')
-        # Copy back the cassandra scripts since profiling may have modified it the previous time
-        shutil.copy(launch_bin, self.get_bin_dir())
-        launch_bin = common.join_bin(self.get_path(), 'bin', 'cassandra')
+        launch_bin = self.get_launch_bin()
 
         # If Windows, change entries in .bat file to split conf from binaries
         if common.is_win():
@@ -572,11 +578,18 @@ class Node(object):
 
         env = self.get_env()
 
+        extension.append_to_server_env(self, env)
+
         if common.is_win():
             self._clean_win_jmx()
 
         pidfile = os.path.join(self.get_path(), 'cassandra.pid')
-        args = [launch_bin, '-p', pidfile, '-Dcassandra.join_ring=%s' % str(join_ring)]
+        args = [launch_bin]
+
+        self.add_custom_launch_arguments(args)
+
+        args = args + ['-p', pidfile, '-Dcassandra.join_ring=%s' % str(join_ring)]
+
         args.append('-Dcassandra.logdir=%s' % os.path.join(self.get_path(), 'logs'))
         if replace_token is not None:
             args.append('-Dcassandra.replace_token=%s' % str(replace_token))
@@ -764,6 +777,7 @@ class Node(object):
     def bulkload(self, options):
         loader_bin = common.join_bin(self.get_path(), 'bin', 'sstableloader')
         env = self.get_env()
+        extension.append_to_client_env(self, env)
         # CASSANDRA-8358 switched from thrift to binary port
         host, port = self.network_interfaces['thrift'] if self.get_cassandra_version() < '2.2' else self.network_interfaces['binary']
         args = ['-d', host, '-p', str(port)]
@@ -811,12 +825,16 @@ class Node(object):
             cqlsh_options = []
         cqlsh = self.get_tool('cqlsh')
         env = self.get_env()
+        extension.append_to_client_env(self, env)
         host = self.network_interfaces['thrift'][0]
         if self.get_base_cassandra_version() >= 2.1:
             port = self.network_interfaces['binary'][1]
         else:
             port = self.network_interfaces['thrift'][1]
-        args = cqlsh_options + [host, str(port)]
+        args = []
+        args += cqlsh_options
+        extension.append_to_cqlsh_args(self, env, args)
+        args += [host, str(port)]
         sys.stdout.flush()
 
         if cmds is None:

commit e0c2c6f7b737c19c3238ac68b750d261eb9c8064

Fix indentation in _stop_agent
Commit f9c70c0 introduced an indentation error whereby the variable
pidfile was referenced without being defined. This solves issue #565.

ccmlib/dse_node.py
@@ -154,16 +154,16 @@ class DseNode(Node):
         agent_dir = os.path.join(self.get_path(), 'datastax-agent')
         if os.path.exists(agent_dir):
             pidfile = os.path.join(agent_dir, 'datastax-agent.pid')
-        if os.path.exists(pidfile):
-            with open(pidfile, 'r') as f:
-                pid = int(f.readline().strip())
-                f.close()
-            if pid is not None:
-                try:
-                    os.kill(pid, signal.SIGKILL)
-                except OSError:
-                    pass
-            os.remove(pidfile)
+            if os.path.exists(pidfile):
+                with open(pidfile, 'r') as f:
+                    pid = int(f.readline().strip())
+                    f.close()
+                if pid is not None:
+                    try:
+                        os.kill(pid, signal.SIGKILL)
+                    except OSError:
+                        pass
+                os.remove(pidfile)
 
     def nodetool(self, cmd, username=None, password=None, capture_output=True, wait=True):
         if password is not None: