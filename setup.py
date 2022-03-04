7e5ff6af02ac74c32c4380e1d1690b23e2a2c334
mck <mck@apache.org>
2022-03-04 21:08:37 +0100

    Introduce pbr for auto-versioning patch by Michael Semb Wever, Sam Tunnicliffe; reviewed by Sam Tunnicliffe and Ekaterina Dimitrova for CASSANDRA-17351

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from os.path import abspath, dirname, join
-from platform import system
 from shutil import copyfile
 
 try:
@@ -9,36 +7,9 @@ try:
 except ImportError:
     from distutils.core import setup
 
-ccmscript = 'ccm'
-if system() == "Windows":
-    copyfile('ccm', 'ccm.py')
-    ccmscript = 'ccm.py'
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+copyfile('ccm', 'ccm.py')
 
 setup(
-    name='ccm',
-    version='3.1.6',
-    description='Cassandra Cluster Manager',
-    author='Sylvain Lebresne',
-    author_email='sylvain@datastax.com',
-    url='https://github.com/riptano/ccm',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=['ccmlib', 'ccmlib.cmds'],
-    scripts=[ccmscript],
-    install_requires=['pyYaml', 'six >=1.4.1', 'psutil'],
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ],
-
+    setup_requires=['pbr>=5.8.1'],
+    pbr=True,
 )
