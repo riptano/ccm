dd1c2ddb5d5790e205fb363c519cf64999fcf44f
Ariel Weisberg <aweisberg@apple.com>
2022-11-11 09:28:09 -0500

    Fix broken nosetest to pytest fixtures

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 import sys
 
 
@@ -8,16 +6,3 @@ TEST_DIR = "test-dir"
 
 if sys.version_info < (3, 0):
     FileNotFoundError = OSError
-
-
-def setup_package():
-    try:
-        shutil.rmtree(TEST_DIR)
-    except FileNotFoundError:
-        pass
-
-    os.makedirs(TEST_DIR)
-
-
-def teardown_package():
-    shutil.rmtree(TEST_DIR)
